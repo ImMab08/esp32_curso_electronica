3977dc9e384b512dced42ee4ba447834bdf27a0e
mab08 <frankyvargas15@gmail.com>
2026-02-28 10:08:12 -0500

    Clase 28/02/2026

--- esp32_comunicacion_por_bluetooth/src/main.cpp
+++ esp32_comunicacion_por_bluetooth/src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <NewPing.h>
+#include <BluetoothSerial.h>
 
 constexpr int PIN_ECHO = 21;
 constexpr int PIN_TRIG = 19;
@@ -7,6 +8,9 @@ constexpr int BUZZER = 18;
 
 constexpr int MAX_DISTANCE = 200; // cm
 constexpr int DISTANCIA_MAX = 50; // cm
+
+// Bluetooth
+BluetoothSerial btSerial;
 
 NewPing sonar(PIN_TRIG, PIN_ECHO, MAX_DISTANCE);
 
@@ -19,56 +23,59 @@ void beep(int delayTime) {
 
 void setup() {
 
-  Serial.begin(115200);
-  pinMode(BUZZER, OUTPUT);
+  // Serial.begin(115200);
+  // pinMode(BUZZER, OUTPUT);
+  btSerial.begin("Programando ando");
+  Serial.println("Iniciando bluetooth...");
 
 }
 
 void loop() {
+  btSerial.println("Hola mundo desde ESP32");
 
-  delay(500);
-  int distance = sonar.ping_cm();
+  // delay(500);
+  // int distance = sonar.ping_cm();
 
-  if (distance == 0)
-  {
-    Serial.println("Fuera de rango");
-    digitalWrite(BUZZER, LOW);
-    return;
-  }
+  // if (distance == 0)
+  // {
+  //   Serial.println("Fuera de rango");
+  //   digitalWrite(BUZZER, LOW);
+  //   return;
+  // }
 
-  Serial.print("Distancia: ");
-  Serial.print(distance);
-  Serial.println(" cm");
+  // Serial.print("Distancia: ");
+  // Serial.print(distance);
+  // Serial.println(" cm");
 
-  if (distance <= 3)
-  {
-    // Pitido constante (muy cerca)
-    digitalWrite(BUZZER, HIGH);
-  }
-  else if (distance <= 10)
-  {
-    // Muy rápido
-    beep(50);
-  }
-  else if (distance <= 20)
-  {
-    // Rápido
-    beep(150);
-  }
-  else if (distance <= 35)
-  {
-    // Medio
-    beep(300);
-  }
-  else if (distance <= DISTANCIA_MAX)
-  {
-    // Lento
-    beep(600);
-  }
-  else
-  {
-    // Lejos → silencio
-    digitalWrite(BUZZER, LOW);
-    delay(100);
-  }
+  // if (distance <= 3)
+  // {
+  //   // Pitido constante (muy cerca)
+  //   digitalWrite(BUZZER, HIGH);
+  // }
+  // else if (distance <= 10)
+  // {
+  //   // Muy rápido
+  //   beep(50);
+  // }
+  // else if (distance <= 20)
+  // {
+  //   // Rápido
+  //   beep(150);
+  // }
+  // else if (distance <= 35)
+  // {
+  //   // Medio
+  //   beep(300);
+  // }
+  // else if (distance <= DISTANCIA_MAX)
+  // {
+  //   // Lento
+  //   beep(600);
+  // }
+  // else
+  // {
+  //   // Lejos → silencio
+  //   digitalWrite(BUZZER, LOW);
+  //   delay(100);
+  // }
 }
