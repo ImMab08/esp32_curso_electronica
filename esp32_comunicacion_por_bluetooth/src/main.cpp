52d13af521cea19f280e98fb5cef7510ee9ba8a2
mab08 <frankyvargas15@gmail.com>
2026-03-05 23:38:11 -0500

    Clase 28/02/2026

--- esp32_comunicacion_por_bluetooth/src/main.cpp
+++ esp32_comunicacion_por_bluetooth/src/main.cpp
@@ -23,8 +23,8 @@ void beep(int delayTime) {
 
 void setup() {
 
-  // Serial.begin(115200);
-  // pinMode(BUZZER, OUTPUT);
+  Serial.begin(115200);
+  pinMode(BUZZER, OUTPUT);
   btSerial.begin("Programando ando");
   Serial.println("Iniciando bluetooth...");
 
@@ -33,49 +33,49 @@ void setup() {
 void loop() {
   btSerial.println("Hola mundo desde ESP32");
 
-  // delay(500);
-  // int distance = sonar.ping_cm();
+  delay(500);
+  int distance = sonar.ping_cm();
 
-  // if (distance == 0)
-  // {
-  //   Serial.println("Fuera de rango");
-  //   digitalWrite(BUZZER, LOW);
-  //   return;
-  // }
+  if (distance == 0)
+  {
+    Serial.println("Fuera de rango");
+    digitalWrite(BUZZER, LOW);
+    return;
+  }
 
-  // Serial.print("Distancia: ");
-  // Serial.print(distance);
-  // Serial.println(" cm");
+  Serial.print("Distancia: ");
+  Serial.print(distance);
+  Serial.println(" cm");
 
-  // if (distance <= 3)
-  // {
-  //   // Pitido constante (muy cerca)
-  //   digitalWrite(BUZZER, HIGH);
-  // }
-  // else if (distance <= 10)
-  // {
-  //   // Muy rápido
-  //   beep(50);
-  // }
-  // else if (distance <= 20)
-  // {
-  //   // Rápido
-  //   beep(150);
-  // }
-  // else if (distance <= 35)
-  // {
-  //   // Medio
-  //   beep(300);
-  // }
-  // else if (distance <= DISTANCIA_MAX)
-  // {
-  //   // Lento
-  //   beep(600);
-  // }
-  // else
-  // {
-  //   // Lejos → silencio
-  //   digitalWrite(BUZZER, LOW);
-  //   delay(100);
-  // }
+  if (distance <= 3)
+  {
+    // Pitido constante (muy cerca)
+    digitalWrite(BUZZER, HIGH);
+  }
+  else if (distance <= 10)
+  {
+    // Muy rápido
+    beep(50);
+  }
+  else if (distance <= 20)
+  {
+    // Rápido
+    beep(150);
+  }
+  else if (distance <= 35)
+  {
+    // Medio
+    beep(300);
+  }
+  else if (distance <= DISTANCIA_MAX)
+  {
+    // Lento
+    beep(600);
+  }
+  else
+  {
+    // Lejos → silencio
+    digitalWrite(BUZZER, LOW);
+    delay(100);
+  }
 }
--- esp32_mostrar_en_pantalla/src/main.cpp
+++ esp32_mostrar_en_pantalla/src/main.cpp
@@ -1,18 +1,109 @@
 #include <Arduino.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_ST7735.h>
+#include <NewPing.h>
+#include <SPI.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// Definir pines - Sensor
+constexpr int PIN_TRIG = 12; // Envia un ultrasonido
+constexpr int PIN_ECHO = 13; // Recibe al sensor
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+constexpr int MAX_DISTANCE = 200; // cm
+constexpr int DISTANCE_MAX = 50;  // cm
+
+// Crear objeto para medir distancia
+NewPing sonar(PIN_TRIG, PIN_ECHO, MAX_DISTANCE);
+
+// Definir los pines - Pantalla
+constexpr int TFT_CS = 5;
+constexpr int TFT_DC = 2;
+constexpr int TFT_RES = 4;
+constexpr int TFT_SDA = 23;
+constexpr int TFT_SCL = 18;
+
+// Crear objeto en pantalla
+Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RES);
+
+// Control de tiempo
+unsigned long lastUpdate = 0;
+const unsigned long interval = 150;
+
+void dibujarBarrita(int distancia)
+{
+    if (distancia > DISTANCE_MAX)
+        distancia = DISTANCE_MAX;
+
+    int anchoBarrita = map(distancia, 0, DISTANCE_MAX, 140, 0);
+
+    // Color según próximidad
+    uint16_t color;
+    if (distancia < 10)
+        color = ST77XX_RED;
+    else if (distancia < 25)
+        color = ST77XX_YELLOW;
+    else
+        color = ST77XX_GREEN;
+
+    // Fondo de la barra (marco)
+    tft.drawRect(10, 70, 140, 20, ST77XX_BLACK);
+
+    // Limpiar inferior
+    tft.fillRect(11, 71, 138, 18, ST77XX_WHITE);
+
+    // Dibujar barra dinamica
+    tft.fillRect(11, 71, anchoBarrita, 18, color);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void setup()
+{
+
+    Serial.begin(115200);
+    
+    pinMode(PIN_TRIG, OUTPUT);
+    pinMode(PIN_ECHO, INPUT);
+    
+    SPI.begin(18, -1, 23, TFT_CS);
+
+    // Inicializar pantalla
+    tft.initR(INITR_BLACKTAB);
+    
+    // Limpiar pantalla
+    tft.setRotation(1);
+    tft.fillScreen(ST77XX_WHITE);
+
+    // Configurar texto
+    tft.setTextColor(ST77XX_BLACK);
+    tft.setTextSize(2);
+    tft.setCursor(10, 10);
+    tft.println("Distancia: ");
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+{
+
+    unsigned long currentMililis = millis();
+
+    if (currentMililis - lastUpdate >= interval)
+    {
+        lastUpdate = currentMililis;
+        delay(500);
+        int distancia = sonar.ping_cm();
+
+        if (distancia == 0)
+            distancia = MAX_DISTANCE;
+
+        Serial.print("Distancia: ");
+        Serial.println(distancia);
+
+        // Limpiar zona del número
+        tft.fillRect(10, 20, 140, 30, ST77XX_BLACK);
+
+        tft.setCursor(10, 10);
+        tft.setTextColor(ST77XX_WHITE);
+        tft.setTextSize(2);
+        tft.print(distancia);
+        tft.print(" cm");
+
+        dibujarBarrita(distancia);
+    }
 }
